cfd9a7883beda8be56dc8976efa652bba5662d0c
jef-maubert <53878419+jef-maubert@users.noreply.github.com>
2022-10-24 17:55:18 +0200

    start aa

---
+++ aa.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 17:34:57 2022
+
+@author: jef
+"""
+import tkinter as tk
+PADX_STD = 5
+
+class AstroTk(tk.Tk):
+
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.parent = parent
+        self.last_position = tk.StringVar()
+        self.last_position_dt = tk.StringVar()
+        self.current_position = tk.StringVar()
+        self.current_position_dt = tk.StringVar()
+        self.initialize()
+        
+    def initialize(self):
+        self.grid()
+        last_pos_frame = tk.LabelFrame(self, text="Last position", borderwidth=2, relief=tk.GROOVE)
+        last_pos_frame.grid(column=0, row=0, sticky='ENWS', padx=PADX_STD, pady=2)
+        self.btn_modif_last_pos = tk.Button(last_pos_frame, text="modify", command=self.on_button_modif_last_pos)
+        self.btn_modif_last_pos.grid(row=1, column=2, padx=PADX_STD, sticky="EW")
+ 
+        current_pos_frame = tk.LabelFrame(self, text="Current position", borderwidth=2, relief=tk.GROOVE)
+        current_pos_frame.grid(column=0, row=1, sticky='ENWS', padx=PADX_STD, pady=2)
+        self.btn_modif_current_pos = tk.Button(current_pos_frame, text="modify", command=self.on_button_modif_course_and_speed)
+        self.btn_modif_current_pos.grid(row=1, column=2, padx=PADX_STD, sticky="EW")
+
+    def on_button_modif_last_pos(self):
+         return
+
+    def on_button_modif_course_and_speed(self):
+         return
+
+def main () :
+    my_app = AstroTk(None)
+    my_app.mainloop()
+
+if __name__ == "__main__":
+    main()
